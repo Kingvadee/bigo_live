022f388fd2f717fb25f404f478e67895b4982b0d
cool-dave18 <officialdave511>
2025-02-08 02:15:39 +0100

    script for submission of the questionnaire form

---
+++ public/script.js
@@ -0,0 +1,4 @@
+document.getElementById("questionnaire-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for submitting the form! We will review your application shortly.");
+});
